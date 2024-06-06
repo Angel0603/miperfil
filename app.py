08905f2aa9c5ead8184f76fb153cee7dc47c3088
Angel0603 <20211050@uthh.edu.mx>
2024-06-05 20:23:32 -0600

    Agregando carpeta de imagen y otros diseños

--- app.py
+++ app.py
@@ -4,10 +4,37 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    nombre = "Ángel de Jesús Lara Barrera"
-    grado = "9no"
-    grupo = "B"
-    return nombre + "<br>" + grado + "<br>" + grupo
+    return '''
+    <html>
+    <head>
+        <title>Mi Perfil Web con Flask</title>
+    </head>
+    <body>
+        <!-- Mostrar una imagen -->
+        <img src="images/mifoto.png" alt="Mi foto de perfil">
+
+        <!-- Botón para mostrar la información -->
+        <button onclick="mostrarInfo()">Ver información</button>
+
+        <!-- Contenedor donde se mostrará la información -->
+        <div id="info" style="display:none;">
+            <ul>
+                <li>Ángel de Jesús Lara Barrera</li>
+                <li>9no</li>
+                <li>B</li>
+            </ul>
+        </div>
+
+        <script>
+        function mostrarInfo() {
+            // Cambia el estilo del contenedor para mostrarlo cuando se hace clic en el botón
+            var contenedorInfo = document.getElementById('info');
+            contenedorInfo.style.display = 'block';
+        }
+        </script>
+    </body>
+    </html>
+    '''
 
 if __name__ == '__main__':
     app.run(debug=True)
